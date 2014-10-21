2e55246d93517edfd10b31cac1d0ff978dfa9ea7
Mitzi Moncada <nesmones1591@hotmail.com>
2014-10-21 16:36:13 -0500

    archivo

---
+++ archivo/encabezado_archivo.php
@@ -0,0 +1,148 @@
+<h2 id="__fluent__" ><div align="left"></div></h2>
+<div class="ingresos">
+    <div class="fluent-menu" data-role="fluentmenu">
+        <ul class="tabs-holder">
+            <li class="special"><a href="index.php">MODULO DE FACTURACION</a></li>
+            <li class="active"><a href="#tab_home">Inicio</a></li>
+            <li><a href="#Reportes">Reportes</a></li>
+
+        </ul>
+
+        <div class="tabs-content">
+            <div class="tab-panel" id="tab_home">
+                <div class="tab-panel-group">
+                    <div class="tab-group-content">
+                        <div class="tab-content-segment">
+
+                            <button  class="fluent-button" onclick="location.href = 'aseguradoras.php';">
+                                <span class="icon-enter on-left"></span> 
+                                Expedientes solicitados
+
+                            </button>
+
+
+
+                            <button class="fluent-button" onclick="location.href = 'particulares.php';">
+                                <a href="particulares.php"><span  class="icon-enter-2 on-left"></span> 
+                                    Expedientes fuera
+
+                                </a>
+                            </button>
+
+
+
+
+
+                        </div>
+
+                    </div>
+                    <div class="tab-group-caption"></div>
+                </div>
+
+
+
+
+
+
+
+
+
+                <div class="tab-panel-group">
+                    <div class="tab-group-content">
+                        <div class="tab-content-segment">
+                            <button class="fluent-button" onclick="location.href = 'particulares.php';">
+                                <a href="saldoInicial.php"><span  class="icon-cube on-left"></span> 
+                                    Expedientes sin fv
+
+                                </a>
+                            </button>
+
+                            <button class="fluent-button" onclick="location.href = 'particulares.php';">
+                                <a href="cancelarFacturas.php"><span  class="icon-cancel on-left"></span> 
+                                    Bloquear expedientes-seguros
+                                </a>
+                            </button>
+
+
+                        </div>
+
+                    </div>
+                    <div class="tab-group-caption"></div>
+                </div>
+
+
+            </div>
+
+            <div class="tab-panel" id="Reportes">
+                <div class="tab-panel-group">
+                    <div class="tab-group-content">
+                        <div class="tab-content-segment">
+                            <button class="fluent-button" onclick="location.href = 'particulares.php';">
+                                <a href="saldoInicial.php"><span  class="icon-cube on-left"></span> 
+                                    Consulta de expedientes
+                                </a>
+                            </button>
+                            <button class="fluent-button" onclick="location.href = 'particulares.php';">
+                                <a href="saldoInicial.php"><span  class="icon-cube on-left"></span> 
+
+                                    Expedientes nuevos
+                                </a>
+                            </button>
+                            <button class="fluent-button" onclick="location.href = 'particulares.php';">
+                                <a href="saldoInicial.php"><span  class="icon-cube on-left"></span> 
+                                    Expedientes fuera
+                                </a>
+                            </button>
+                            
+                            
+                            
+                            </div>
+
+                    </div>
+                    <div class="tab-group-caption"></div>
+                </div>
+
+
+
+
+
+
+
+
+
+                <div class="tab-panel-group">
+                    <div class="tab-group-content">
+                        <div class="tab-content-segment">
+                            
+                            
+                            
+                            
+                            
+                            <button class="fluent-button" onclick="location.href = 'particulares.php';">
+                                <a href="saldoInicial.php"><span  class="icon-cube on-left"></span> 
+                                    Expedientes sin fv
+                                </a>
+                            </button>
+                            <button class="fluent-button" onclick="location.href = 'particulares.php';">
+                                <a href="saldoInicial.php"><span  class="icon-cube on-left"></span> 
+                                    Expedientes duplicados</a>
+                            </button>
+                            <button class="fluent-button" onclick="location.href = 'particulares.php';">
+                                <a href="saldoInicial.php"><span  class="icon-cube on-left"></span> 
+                                    Eliminar expedientes
+                                </a>
+                            </button>
+
+
+
+
+                        </div>
+
+                    </div>
+                    <div class="tab-group-caption"></div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
